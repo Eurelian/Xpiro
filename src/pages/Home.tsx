fbb090fe51343dcdc9af15058480f4d392ed6aeb
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-09-11 10:10:20 +0200

    fixed date format

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,12 +10,20 @@ import {
 	IonCardTitle,
 	IonCardSubtitle,
 	IonButton,
+	IonGrid,
+	IonCol,
+	IonRow,
+	IonIcon,
 } from "@ionic/react";
+import { trashOutline } from "ionicons/icons";
 import React, { useState, useContext } from "react";
 
 //components
 import AddButton from "../components/addButton";
 import ProductsContext from "../data/products";
+
+//utils
+import handleDate from "../utils/dateConvert";
 
 import "./Home.css";
 
@@ -24,24 +32,46 @@ const Home: React.FC = () => {
 	return (
 		<IonPage>
 			<IonHeader className='ion-no-border header'>
-				<IonToolbar className='toolbar'>
+				<IonToolbar className='toolbar ion-text-center'>
 					<IonTitle>Xpiro</IonTitle>
 				</IonToolbar>
 			</IonHeader>
 			<IonContent>
-				<IonList>
+				<IonList className='list-parent'>
 					{products.map((item) => (
-						<IonItem key={item.id}>
-							<IonCard>
-								<IonCardTitle>{item.groceryName}</IonCardTitle>
-								<IonCardSubtitle>{item.expiryDate}</IonCardSubtitle>
-								<IonButton onClick={() => deleteProduct(item.id)}>
-									Delete
-								</IonButton>
+						<IonItem key={item.id} className='list-item'>
+							<IonCard className='card'>
+								<IonGrid>
+									<IonRow className='ion-justify-content-between'>
+										<IonCol size='8'>
+											<IonCardTitle className='ion-text-capitalize ion-margin-bottom'>
+												{item.groceryName}
+											</IonCardTitle>
+											<IonCardSubtitle>
+												{handleDate(item.expiryDate)}
+											</IonCardSubtitle>
+										</IonCol>
+										<IonCol
+											size='4'
+											style={{
+												display: "flex",
+												alignItems: "center",
+												justifyContent: "flex-end",
+											}}
+										>
+											<IonRow>
+												<IonButton onClick={() => deleteProduct(item.id)}>
+													<IonIcon icon={trashOutline}></IonIcon>
+												</IonButton>
+											</IonRow>
+										</IonCol>
+									</IonRow>
+								</IonGrid>
 							</IonCard>
 						</IonItem>
 					))}
 				</IonList>
+
 				<AddButton></AddButton>
 			</IonContent>
 		</IonPage>
